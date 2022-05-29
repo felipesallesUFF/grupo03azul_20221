04cb7014539b26573dcb9663d35db5d091da3c1a
Felipe Salles <sallesfelipe@id.uff.br>
2022-05-28 21:15:19 -0300

    Initial commit

---
+++ android/assets/maps/TUTsnowtileset.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="TUTsnowtileset" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="TUTsnowtileset.png" width="512" height="512"/>
+ <wangsets>
+  <wangset name="rocks" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="45" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="46" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="47" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="59" wangid="1,1,1,1,1,1,1,0"/>
+   <wangtile tileid="60" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="61" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="62" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="63" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="75" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="76" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="77" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="78" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="79" wangid="1,0,0,0,0,0,1,1"/>
+  </wangset>
+  <wangset name="snow" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="123" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="124" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="139" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="140" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="141" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="142" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="143" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="157" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="159" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="173" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="174" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="175" wangid="1,1,1,1,1,1,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
