59f256cb0ba067530fd19b7cc4aba06c64e494b4
Guilherme Moreira <gfmoreira@id.uff.br>
2022-07-03 17:07:05 -0300

    testando mapas

---
+++ android/assets/maps/mapa3/desert.tsx
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="desert" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="48" columns="8">
+ <image source="desert.png" width="265" height="199"/>
+ <tile id="29" probability="0.95"/>
+ <tile id="30" probability="0.001"/>
+ <tile id="31" probability="0.001"/>
+ <tile id="37" probability="0.001"/>
+ <tile id="38" probability="0.001"/>
+ <tile id="39" probability="0.001"/>
+ <tile id="45" probability="0.001"/>
+ <tile id="46" probability="0.001"/>
+ <tile id="47" probability="0.001"/>
+ <wangsets>
+  <wangset name="stoneWall" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="24" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="25" wangid="0,0,0,1,1,1,0,0"/>
+   <wangtile tileid="26" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="32" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="33" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="34" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="35" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="36" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="40" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="41" wangid="1,1,0,0,0,0,0,1"/>
+   <wangtile tileid="42" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="43" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="44" wangid="0,1,1,1,1,1,0,0"/>
+  </wangset>
+  <wangset name="brickWall" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,1,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="8" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="9" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="10" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="16" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="17" wangid="1,1,0,0,0,0,0,1"/>
+   <wangtile tileid="18" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="19" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="20" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="27" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="28" wangid="0,1,1,1,1,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
