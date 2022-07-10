3165dd9b75138c11b1a895114cf60efbaae9eb00
Guilherme Moreira <gfmoreira@id.uff.br>
2022-07-10 00:19:41 -0300

    menu

--- core/src/br/uff/ic/lek/screens/Menu.java
+++ core/src/br/uff/ic/lek/screens/Menu.java
@@ -6,91 +6,158 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 import br.uff.ic.lek.game.World;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.Input.TextInputListener;
 
 
 import br.uff.ic.lek.Alquimia;
 
 public class Menu implements Screen {
 
-    final Alquimia game;
-    OrthographicCamera camera;
-    int pilar = 1;
-    private Texture texture = new Texture(Gdx.files.internal("img/guerreira3.png"));
-    private Image splashImage = new Image(texture);
-    private Stage stage = new Stage();
+    private SpriteBatch batch;
+    protected Stage stage;
+    private Viewport viewport;
+    private OrthographicCamera camera;
+    private TextureAtlas atlas;
+    protected Skin skin;
+    public boolean check = false;
+    public String nickname;
+    public TextInputListener nick;
 
-    public Menu(final Alquimia gam) {
-        game = gam;
+    public Menu(Alquimia alquimia)
+    {
+        atlas = new TextureAtlas("skin.atlas");
+        skin = new Skin(Gdx.files.internal("skin.json"), atlas);
 
+        batch = new SpriteBatch();
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
+        viewport = new FitViewport(World.getMapWidthPixel(), World.getMapHeightPixel(), camera);
+        viewport.apply();
 
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+
+        stage = new Stage(viewport, batch);
+    }
+
+
+    @Override
+    public void show() {
+        //Stage should controll input:
+        Gdx.input.setInputProcessor(stage);
+
+        //Create Table
+        Table mainTable = new Table();
+        //Set table to fill stage
+        mainTable.setFillParent(true);
+        //Set alignment of contents in the table.
+        mainTable.top();
+
+        //Create buttons
+        TextButton playButton = new TextButton("Play", skin);
+        TextButton optionsButton = new TextButton("Options", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        //Add listeners to buttons
+        playButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ((Game)Gdx.app.getApplicationListener()).setScreen(new PlayScreen());
+                //((Game)Gdx.app.getApplicationListener()).setScreen(new SplashScreen());
+            }
+        });
+        optionsButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                check = true;
+            }
+        });
+        exitButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        //Add buttons to table
+        mainTable.add(playButton);
+        mainTable.row();
+        mainTable.add(optionsButton);
+        mainTable.row();
+        mainTable.add(exitButton);
+
+        //Add table to stage
+        stage.addActor(mainTable);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClearColor(.1f, .12f, .16f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        stage.act();
+        stage.draw();
 
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
-
-        game.batch.begin();
-        this.stage.draw();
-        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
-        if(pilar == 0){
-            game.font.draw(game.batch, "helo world!!! ", 100, 50);
-        }
-        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
-        game.batch.end();
-
-        if (Gdx.input.isTouched()) {
-            //game.setScreen(new SplashScreen());
-            pilar = 0;
-            //dispose();
-        }
-        if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
-            pilar = 1;
+        if(check == true){
+            Gdx.input.getTextInput( nick, "Nickname", nickname, "...");
+            Gdx.input.setOnscreenKeyboardVisible(true);
         }
     }
 
     @Override
     public void resize(int width, int height) {
-    }
-
-    @Override
-    public void show() {
-        this.splashImage.setWidth(Gdx.graphics.getWidth());
-        this.splashImage.setHeight(Gdx.graphics.getHeight());
-        this.stage.addActor(splashImage);
-    }
-
-    @Override
-    public void hide() {
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
     }
 
     @Override
     public void pause() {
+
     }
 
     @Override
     public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void input (String text) {
+        nickname = text;
+    }
+
+    @Override
+    public void canceled () {
+        check = false;
     }
 
     @Override
     public void dispose() {
+        skin.dispose();
+        atlas.dispose();
     }
 }
