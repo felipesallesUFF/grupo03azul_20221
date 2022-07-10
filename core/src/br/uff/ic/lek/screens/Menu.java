36b15e8c70a025d7a0b1879c214b943952008db7
Guilherme Moreira <gfmoreira@id.uff.br>
2022-07-10 19:58:16 -0300

    Update Menu.java

--- core/src/br/uff/ic/lek/screens/Menu.java
+++ core/src/br/uff/ic/lek/screens/Menu.java
@@ -4,36 +4,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 import br.uff.ic.lek.PlayerData;
-import br.uff.ic.lek.game.World;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.badlogic.gdx.Input.TextInputListener;
-import com.sun.tools.javac.util.Constants;
 
 
 import br.uff.ic.lek.Alquimia;
 
+//essa clase é o menu do jogo
 public class Menu implements Screen {
 
     private Alquimia parent;
@@ -43,11 +33,9 @@ public class Menu implements Screen {
     private OrthographicCamera camera;
     private TextureAtlas atlas;
     protected Skin skin;
-    public boolean check = false;
+    //estou pegando os dados do player
     public String nickname = PlayerData.myPlayerData().getPlayerNickName();
     public String email = PlayerData.myPlayerData().getEmail();
-    //public TextField name;
-    //public TextField mail;
     public TextInputListener nick;
     public TextInputListener Email;
 
@@ -57,7 +45,7 @@ public class Menu implements Screen {
         //
         batch = new SpriteBatch();
         camera = new OrthographicCamera();
-        viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
+        viewport = new FitViewport(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2, camera);
         viewport.apply();
 
         camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
@@ -92,27 +80,57 @@ public class Menu implements Screen {
         playButton.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                ((Game)Gdx.app.getApplicationListener()).setScreen(new SplashScreen());
+                ((Game)Gdx.app.getApplicationListener()).setScreen(new SplashScreen());//começar jogo
             }
         });
         NicknameButton.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
+                //Gdx.input.getTextInput( nick, "Nickname", nickname, nickname);
+                nick = new TextInputListener()
+                {
+                    @Override
+                    public void input(String input)//o que ele vai fazer com o texto digitado
+                    {
+                        nickname = input;
+                        PlayerData.myPlayerData().setPlayerNickName(nickname);
+                    }
+
+                    @Override
+                    public void canceled()//o que ele faz se vc cancelar
+                    {
+                    }
+                };
                 Gdx.input.getTextInput( nick, "Nickname", nickname, nickname);
-                Gdx.input.setOnscreenKeyboardVisible(true);
+                Gdx.input.setOnscreenKeyboardVisible(true);//subir o teclado no celular
             }
         });
         EmailButton.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
+                //Gdx.input.getTextInput( Email, "Email", email, email);
+                Email = new TextInputListener()
+                {
+                    @Override
+                    public void input(String input)//o que ele vai fazer com o texto digitado
+                    {
+                        email = input;
+                        PlayerData.myPlayerData().setEmail(email);
+                    }
+
+                    @Override
+                    public void canceled()//o que ele faz se vc cancelar
+                    {
+                    }
+                };
                 Gdx.input.getTextInput( Email, "Email", email, email);
-                Gdx.input.setOnscreenKeyboardVisible(true);
+                Gdx.input.setOnscreenKeyboardVisible(true);//subir o teclado no celular
             }
         });
         exitButton.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                Gdx.app.exit();
+                Gdx.app.exit();//sair do jogo
             }
         });
 
@@ -159,16 +177,6 @@ public class Menu implements Screen {
     @Override
     public void hide() {
 
-    }
-
-    //@Override
-    public void input (String text) {
-        nickname = text;
-    }
-
-    //@Override
-    public void canceled () {
-        check = false;
     }
 
     @Override
